f1d559c50a4ce5816cea5037309b54d9ad502b4e
jsti_ccz <jsti_ccz@163.com>
2022-02-11 17:07:40 +0800

    增加路由提交

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
 import HelloWorld from 'comps/HelloWorld.vue'
@@ -6,8 +7,21 @@ console.log(import.meta.env)
 </script>
 
 <template>
-  <img alt="Vue logo" src="@/assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <p>
+    <!--使用 router-link 组件进行导航 -->
+    <!--通过传递 `to` 来指定链接 -->
+    <!--`<router-link>` 将呈现一个带有正确 `href` 属性的 `<a>` 标签-->
+    <router-link to="/home">Go to Home</router-link>
+    <router-link to="/about">Go to About</router-link>
+  </p>
+  <!-- 路由出口 -->
+  <!-- 路由匹配到的组件将渲染在这里 -->
+  <div>
+    <router-view></router-view>
+  </div>
+  
+  <!-- <img alt="Vue logo" src="@/assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" /> -->
 </template>
 
 <style>
--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,11 @@ const {
 } = require('path')
 import path from "path";
 import vue from '@vitejs/plugin-vue'
+import { viteMockServe } from 'vite-plugin-mock'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [vue(),viteMockServe({ supportTs: false })],
   resolve: {
     alias: [ //配置别名
       {
@@ -18,9 +19,26 @@ export default defineConfig({
       },
       {
         find: 'comps',
-        replacement: resolve(__dirname, 'src/components')       
+        replacement: resolve(__dirname, 'src/components')
+      },
+      {
+        find: 'views',
+        replacement: resolve(__dirname, 'src/views')
       }
-      
+
     ]
-  },
+  },  
+  // resolve: {
+  //   alias: {
+  //     "@": path.resolve(__dirname, "src"),
+  //     comps: path.resolve(__dirname, "src/components"),
+  //     styles: path.resolve(__dirname, "src/styles"),
+  //     plugins: path.resolve(__dirname, "src/plugins"),
+  //     views: path.resolve(__dirname, "src/views"),
+  //     layouts: path.resolve(__dirname, "src/layouts"),
+  //     utils: path.resolve(__dirname, "src/utils"),
+  //     apis: path.resolve(__dirname, "src/apis"),
+  //     dirs: path.resolve(__dirname, "src/directives"),
+  //   }
+  // },
 })
